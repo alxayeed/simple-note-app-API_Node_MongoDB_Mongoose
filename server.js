d9dbc9a4cdd0e05a268d02a515dddffb375375f2
alxayeed <alxayeed@gmail.com>
2020-01-25 18:57:09 +0600

    Database configured

---
+++ server.js
@@ -0,0 +1,37 @@
+//import modules
+const express = require('express');
+const bodyParser = require('body-parser')
+
+//create express app
+const app = express()
+
+//Setting up middleware
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.use(bodyParser.json())
+
+//configuring Database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+//connecting to the database
+mongoose.connect(dbConfig.url, {
+	useNewUrlParser: true,useUnifiedTopology: true
+}).then(() => {
+	console.log("Sucesfully connected to the database");
+}).catch(err => {
+	console.log("Database connection failed.Exiting now...",err);
+	process.exit();
+});
+
+
+//defining route
+app.get('/',(req, res) => {
+	res.json({"message":"An easy note app,built with NodeJs,MongoDB"})
+});
+
+app.listen(2020,() =>{
+	console.log("server is up and running on port 2020")
+});
